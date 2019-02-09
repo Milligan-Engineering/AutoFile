63e54728f2186fa29dba1bbd51361680b541d84e
Timothy Thacker <tnt.thacker@gmail.com>
2019-02-08 21:48:28 -0500

    Redoing the program without a text file for input, storage, and output. Arrays will totally replace this.

--- AutoFile/AutoFileMain.cpp
+++ AutoFile/AutoFileMain.cpp
@@ -1,5 +1,16 @@
+//File Name: AutoFileMain.cpp
+//Author: Timothy Thacker
+//Email Addresss: tnthacker@my.milligan.edu
+//Assignment Number: Term Project
+//Description: Eventually, this program will take an input "tag" and find files corresponding to said tag, and then automatically file those for the user.
+//Last Changed: February 8, 2019
+
+
+//For now, I will be removing all code in regards to the file and solely work with arrays for better data control and manipulation
+//Thus, this is essentially a rebuild from the ground up. No files.
+
+
 #include <iostream>
-#include <fstream>
 #include <string>
 
 
@@ -7,58 +18,52 @@ using namespace std;
 
 
 int main()
-{ 
+{
 
-	string tagOutput;
-	string tag1;
-	string tag2;
-	string tag3;
-	int tagsCounter = 0;
+	int choice;
+	const int MAX_TAGS = 10;
+	string sortingArray[MAX_TAGS];
 
-
-	cout << "Please input your first tag: ";
-	getline(cin, tag1); //Here is the text output, followed by the Tag string input.
-	cout << "\n";
-	cout << "Please input your second tag: ";
-	getline(cin, tag2);
-	cout << "\n";
-	cout << "Please input your third tag: ";
-	getline(cin, tag3);
-
-
-	fstream tagsFile;
-	tagsFile.open("AutoFileTags.txt", fstream::app); //The input tag has been written to the text file.
-	tagsFile << tag1 << "\n" << tag2 << "\n" << tag3 << "\n";
-	
-	tagsFile.close(); //The text file has been closed.
-	
-
-	tagsFile.open("AutoFileTags.txt"); //The text file has been opened.
-	if (tagsFile.is_open())
+	do
 	{
-		cout << "\nYour current tags are ";
-		while (getline(tagsFile, tagOutput)) //Read the text file, storing the information in the file as string tagOutput
+		cout << "Welcome to AutoFile! \n"
+			<< "Enter 1 to view current tags. \n"
+			<< "Enter 2 to enter new tags. \n"
+			<< "Enter 3 to close the program. \n \n"
+			<< "Submit your choice and press Return: ";
+		cin >> choice;
+
+		switch (choice)
 		{
-			cout << tagOutput; //Here is the text output with the information read from the file.
-			cout << ", ";
-			tagsCounter++;
+		case 1:
+			
+
+		case 2:
+
+
 		}
-		tagsFile.close();
-	}
 
-	else cout << "Program has encountered an error";
+	} while (choice != 3);
 
-	cout << "\nYou have " << tagsCounter << " tags stored currently";
+
+
+
+
+
+
+
+
+
 
 	//keep the window open until the user closes
 	string closeString;
 	cout << "\nEnter anything to close";
-	cin >> closeString; 
+	cin >> closeString;
 
 
 
 
 	return 0;
 
-	
+
 }
