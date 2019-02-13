a1c17827ebe78798a591009cc8207122cc78de36
Timothy Thacker <tnt.thacker@gmail.com>
2019-02-13 10:16:24 -0500

    Adding some content to the cases and a break for each.

--- AutoFile/AutoFileMain.cpp
+++ AutoFile/AutoFileMain.cpp
@@ -3,7 +3,7 @@
 //Email Addresss: tnthacker@my.milligan.edu
 //Assignment Number: Term Project
 //Description: Eventually, this program will take an input "tag" and find files corresponding to said tag, and then automatically file those for the user.
-//Last Changed: February 8, 2019
+//Last Changed: February 11, 2019
 
 
 //For now, I will be removing all code in regards to the file and solely work with arrays for better data control and manipulation
@@ -21,7 +21,7 @@ int main()
 {
 
 	int choice;
-	const int MAX_TAGS = 10;
+	const int MAX_TAGS = 11;
 	string sortingArray[MAX_TAGS];
 
 	do
@@ -36,11 +36,33 @@ int main()
 		switch (choice)
 		{
 		case 1:
+
+
+			for (int arrayCounter1 = 1; arrayCounter1 <= MAX_TAGS; arrayCounter1++) 
+			{
+				
+				cout << "Tag " << arrayCounter1 << " is ";
+				cout << sortingArray[(arrayCounter1-1)];
+				cout << "\n";
+				
+			}
+				
+			break;
+			
 			
 
 		case 2:
 
+			for (int arrayCounter2 = 1; arrayCounter2 <= MAX_TAGS; arrayCounter2++)
+			{
+				cout << "Enter Tag " << arrayCounter2 << " here: ";
+				string tempTag;
+				cin >> tempTag;
+				sortingArray[(arrayCounter2 - 1)] = tempTag;
 
+			}
+
+			break;
 		}
 
 	} while (choice != 3);
